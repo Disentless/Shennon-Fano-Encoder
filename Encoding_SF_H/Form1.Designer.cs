a70a46c0f811fb20b17242ad2728ae14f830e411
James Brown <dim5843@gmail.com>
2017-06-26 17:37:21 +0300

    Initial commit

---
+++ Encoding_SF_H/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Encoding_SF_H
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Shennon-Fano";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 251);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(165, 69);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(146, 251);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(317, 69);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(146, 251);
+            this.listBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Haffman";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(165, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Save to txt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(317, 46);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Save to txt";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 7, 0);
+            this.label1.Size = new System.Drawing.Size(146, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Encoding:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 326);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Encoding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Encoding_SF_H/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encoding_SF_H
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Tree GetChild(Stack<KeyValuePair<string, float>> data)
+        {
+            if (data.Count == 1)
+            {
+                return new Tree(data.Peek().Key);
+            }
+            var sum = data.Sum((x) => { return x.Value; });
+            var half = sum / 2;
+
+            var leftS = new Stack<KeyValuePair<string, float>>();
+            leftS.Push(data.Pop());
+
+            var firstSum = leftS.Peek().Value;
+            while (data.Count > 1)
+            {
+                var top = data.Peek();
+                if (firstSum + top.Value > half + top.Value / 2)
+                {
+                    break;
+                }
+                firstSum += top.Value;
+                leftS.Push(top);
+                data.Pop();
+            }
+
+            var result = new Tree();
+            result.Left = GetChild(leftS);
+            result.Right = GetChild(data);
+
+            return result;
+        }
+
+        private Dictionary<string, string> GetEncoded(Tree root)
+        {
+            var res = new Dictionary<string, string>();
+            if (root.Signal != null)
+            {
+                res.Add(root.Signal, "");
+                return res;
+            }
+
+            var left = GetEncoded(root.Left);
+            var right = GetEncoded(root.Right);
+
+            foreach (var node in left)
+            {
+                res.Add(node.Key, node.Value + "0");
+            }
+            foreach (var node in right)
+            {
+                res.Add(node.Key, node.Value + "1");
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Shennon-Fano encoding.
+        /// </summary>
+        /// <param name="data">"Array" of data.</param>
+        /// <returns>Encoded sequences.</returns>
+        private Dictionary<string, string> encode_SF(Stack<KeyValuePair<string, float>> data)
+        {
+            // Shennon-Fano encoding
+            // Split in half over and over again
+
+            var treeRoot = GetChild(data);
+
+            return GetEncoded(treeRoot);
+        }
+
+        private List<KeyValuePair<string, float>> Data;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files (*.txt)|*.txt";
+            switch (dialog.ShowDialog())
+            {
+                case DialogResult.OK: break;
+                default: return;
+            }
+
+            Data = new List<KeyValuePair<string, float>>();
+            var input = new System.IO.StreamReader(dialog.FileName);
+            while (!input.EndOfStream)
+            {
+                var line = input.ReadLine();
+                var match = System.Text.RegularExpressions.Regex.Match(line, @"^(.+)[\s\t](.+)$");
+                if (!match.Success)
+                {
+                    MessageBox.Show("Corrupted data!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    input.Close();
+                    return;
+                }
+                try
+                {
+                    var signal = match.Groups[1].Value;
+                    var probability = float.Parse(match.Groups[2].Value);
+
+                    Data.Add(new KeyValuePair<string, float>(signal, probability));
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Ошибка чтения данных. Системный разделитель (" + System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator + ") не соответствует использованному.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    input.Close();
+                    return;
+                }
+            }
+            input.Close();
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+
+            foreach (var element in Data)
+            {
+                listBox1.Items.Add(element.Key + "\t|\t" + element.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Data == null) return;
+            listBox2.Items.Clear();
+            var sortedStackOfData = new Stack<KeyValuePair<string, float>>(Data.OrderBy((x) =>
+            {
+                return x.Value;
+            }));
+
+            var encoded = encode_SF(sortedStackOfData);
+            var sorted = encoded.OrderBy((x) => { return x.Key; });
+            foreach (var element in sorted)
+            {
+                listBox2.Items.Add(element.Key + "\t|\t" + element.Value);
+            }
+            if (checkBox1.Checked)
+            {
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "Text file (*.txt)|*.txt";
+                switch (dialog.ShowDialog())
+                {
+                    default: return;
+                    case DialogResult.OK:
+                        break;
+                }
+
+                var output = new System.IO.StreamWriter(dialog.FileName);
+                foreach (var element in sorted)
+                {
+                    output.WriteLine(element.Key + "\t" + element.Value);
+                }
+                output.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Data == null) return;
+            listBox3.Items.Clear();
+
+            var leaves = new List<Tree>();
+            foreach (var el in Data)
+            {
+                var tr = new Tree(el.Key);
+                tr.Value = el.Value;
+                leaves.Add(tr);
+            }
+            Comparison<Tree> comparer = (a, b) => { return a.Value > b.Value ? 1 : -1; };
+            leaves.Sort(comparer);
+
+            while (leaves.Count > 1)
+            {
+                var newLeaf = new Tree();
+                newLeaf.Value = leaves[0].Value + leaves[1].Value;
+                newLeaf.Left = leaves[0];
+                newLeaf.Right = leaves[1];
+
+                leaves.RemoveAt(0);
+                leaves.RemoveAt(0);
+
+                leaves.Add(newLeaf);
+                leaves.Sort(comparer);
+            }
+
+            var res = GetEncoded(leaves[0]);
+            var sorted = res.OrderBy((x) => { return x.Key; });
+            foreach (var element in sorted)
+            {
+                listBox3.Items.Add(element.Key + "\t|\t" + element.Value);
+            }
+            if (checkBox2.Checked)
+            {
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "Text file (*.txt)|*.txt";
+                switch (dialog.ShowDialog())
+                {
+                    default: return;
+                    case DialogResult.OK:
+                        break;
+                }
+
+                var output = new System.IO.StreamWriter(dialog.FileName);
+                foreach (var element in sorted)
+                {
+                    output.WriteLine(element.Key + "\t" + element.Value);
+                }
+                output.Close();
+            }
+        }
+    }
+}
